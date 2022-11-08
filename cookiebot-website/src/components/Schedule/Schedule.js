0cda743a533775246284e291505fe3799723f7d2
Rafael Katri <rkatri20033@gmail.com>
2022-11-08 19:03:58 -0300

    Backend integration

--- cookiebot-website/src/components/Schedule/Schedule.js
+++ cookiebot-website/src/components/Schedule/Schedule.js
@@ -1,15 +1,23 @@
 import Day from "./Day";
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 function Schedule(){
 
     const [data, setData] = useState([]);
 
-    useEffect(() => {
-        fetch('http://localhost:3001/posts/')
-        .then(res => res.json())
-        .then(data => setData(data));
-    }, []);
+    const atualizaNotas = () => {
+        axios
+        .get("http://projeto-2-backend-cookiebot.herokuapp.com/posts",{headers: {"Content-Type": "application/json", "Accept": "application/json", "Access-Control-Allow-Origin": "*"}})
+        .then((response) => {
+          console.log(response.data);
+          setData(response.data);
+        })
+      }
+    
+      useEffect(()=>{
+        atualizaNotas();
+      }, []);
 
     return (
         <div className="data">
