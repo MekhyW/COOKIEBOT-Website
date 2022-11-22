558095b12b2a93fa7d439bbbf1494c75504c8476
Rafael Katri <rkatri20033@gmail.com>
2022-11-22 11:22:45 -0300

    minecraft-like animation

--- src/components/landingPage/Intro.js
+++ src/components/landingPage/Intro.js
@@ -1,12 +1,31 @@
+import {useEffect, useState} from 'react';
+
 
 function Intro(){
+
+    const frases = ["O bot mais supimpa do mercado!", "O bot mais legal do mercado!", "O bot mais legal do mundo!"];
+
+    const [frase, setFrase] = useState(frases[0]);
+
+    function choose(choices) {
+        var index = Math.floor(Math.random() * choices.length);
+        setFrase(choices[index]);
+      }
+
+      useEffect(() => {
+        const interval = setInterval(() => {
+          choose(frases);
+        }, 5000);
+      
+        return () => clearInterval(interval);
+      }, []);
 
     return (
         <>
             <div className="intro">
                 <img src="landing_page_temporary.jpg" alt="logo"/>
                 <h1> CookieBot </h1>
-                <p>O bot mais supimpa do mercado!</p>
+                <p>{frase}</p>
             </div>
         </>
     );
