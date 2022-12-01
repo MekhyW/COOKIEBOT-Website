0fb5f85500ee07336f827d372474a8c57fca3a1b
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2022-11-30 22:25:22 -0300

    Real bot icon

--- src/components/utils/Appbar.js
+++ src/components/utils/Appbar.js
@@ -4,7 +4,7 @@ function Appbar(){
     return (
         <>
             <div className="appbar">
-                <a href="/" id="voltar"><img src='fortunecookiebot.png'></img></a>
+                <a href="/" id="voltar"><img src='cookiebot.jpg'></img></a>
                 <a href="/settings">Settings</a>
                 <a href="/schedule">Schedule</a>
             </div>
