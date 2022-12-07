6a54ef9ba09af65dd11687ac6d437b9d59edb5f6
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2022-12-07 20:44:40 -0300

    Chats

--- src/components/landingPage/Bullet.js
+++ src/components/landingPage/Bullet.js
@@ -5,20 +5,70 @@ function Bullet(){
         <>
             <div className="bullet">
                 <div className="bullet-text">
-                    <img src="super-friends.jpg" alt="logo" />
-                    <h2> Sociedade dos Super Amigos </h2>
-                    <p>Super legal</p>
+                    <img src="bff.jpg" alt="logo" />
+                    <h2> Brasil FurFest </h2>
                 </div>
                 <div className="bullet-text">
-                    <img src="Rosquinha.webp" alt="logo" />
-                    <h2> The One True Donut Assembly </h2>
-                    <p>Meetings are on weekdays on Moe's bar</p>
+                    <img src="furcity.jpg" alt="logo" />
+                    <h2> 🔰FurCity🔰 </h2>
                 </div>
                 <div className="bullet-text">
-                    <img src="illuminati.jpg" alt="logo" />
-                    <h2> Illuminati </h2>
-                    <p>Shhhh....</p>
+                    <img src="artdivulge.jpg" alt="logo" />
+                    <h2> 🍁Art Divulge🍁 </h2>
                 </div>
+                <div className="bullet-text">
+                    <img src="fursitio.jpg" alt="logo" />
+                    <h2> Fursitio </h2>
+                </div>
+                <div className="bullet-text">
+                    <img src="vrfurries.jpg" alt="logo" />
+                    <h2> VRfurs </h2>
+                </div>
+                <div className="bullet-text">
+                    <img src="furriesdesp.jpg" alt="logo" />
+                    <h2> Furries de São Paulo </h2>
+                </div>
+                <div className="bullet-text">
+                    <img src="furrybr2.jpg" alt="logo" />
+                    <h2> Furry Brasil 2.0 </h2>
+                </div>
+                <div className="bullet-text">
+                    <img src="fursantos.jpg" alt="logo" />
+                    <h2> FurSantos </h2>
+                </div>
+                <div className="bullet-text">
+                    <img src="furvenda.jpg" alt="logo" />
+                    <h2> 💲FurVenda💲 </h2>
+                </div>
+                <div className="bullet-text">
+                    <img src="fursuitersdobr.jpg" alt="logo" />
+                    <h2> Fursuiters do Brasil </h2>
+                </div>
+                <div className="bullet-text">
+                    <img src="matilha.jpg" alt="logo" />
+                    <h2> Matilha </h2>
+                </div>
+                <div className="bullet-text">
+                    <img src="furgoias.jpg" alt="logo" />
+                    <h2> FurGoiás </h2>
+                </div>
+                <div className="bullet-text">
+                    <img src="purrtech.jpg" alt="logo" />
+                    <h2> PurrTech! </h2>
+                </div>
+                <div className="bullet-text">
+                    <img src="furpark.jpg" alt="logo" />
+                    <h2> Furpark 🐺 </h2>
+                </div>
+                <div className="bullet-text">
+                    <img src="rof.jpg" alt="logo" />
+                    <h2> ROF </h2>
+                </div>
+                <div className="bullet-text">
+                    <img src="furfamilia.jpg" alt="logo" />
+                    <h2> FurFamília </h2>
+                </div>
+                <h2>And over 100 more Chats...</h2>
             </div>
         </>
     );
